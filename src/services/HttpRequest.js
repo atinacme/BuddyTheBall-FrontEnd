7cb05dcc026a310ef0230f96a17d930af64e4cfd
AIR <air@192.168.1.29>
2023-07-27 17:42:31 +0530

    changes commited for the null request services

--- src/services/HttpRequest.js
+++ src/services/HttpRequest.js
@@ -1,18 +1,34 @@
 import axios from "axios";
 
 const HttpRequest = async (method, url, data) => {
-    const response = await axios({
-        method: method,
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "X-Requested-With": "XMLHttpRequest",
-        },
-        url: url,
-        data: JSON.stringify(data),
-    });
-    return response.data;
+        
+        if(data==null){
+            const response = await axios({
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                url: url
+            });
+            return response.data;
+        } else{
+            const response = await axios({
+                method: method,
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "X-Requested-With": "XMLHttpRequest",
+                },
+                url: url,
+                data: JSON.stringify(data),
+            });
+            return response.data;
+        }
+    
+       
+    
+   
 };
 
 export default HttpRequest;
